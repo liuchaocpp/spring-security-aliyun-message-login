b444d44163848888d99087c7208f8afeb2f6069b
liuchaoGlobal <491938644@qq.comGlobal>
2021-01-24 19:18:17 +0800

    second-commit

--- spring-security/src/main/java/com/jxf/security/controller/SecurityController.java
+++ spring-security/src/main/java/com/jxf/security/controller/SecurityController.java
@@ -1,17 +1,21 @@
 package com.jxf.security.controller;
 
 import com.jxf.security.security.utils.R;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/success")
-    public R successLogin(){
+    public R success(){
         System.out.println("success");
         return R.success().put("success","success");
+    }
+
+    @GetMapping("/error")
+    public R error(){
+        System.out.println("error");
+        return R.error().put("error","error");
     }
 }
